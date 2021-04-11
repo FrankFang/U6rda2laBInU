435c768b0006515637a402185aab1288180fbf1f
frankfang <frankfang1990@gmail.com>
2021-04-12 04:18:43 +0800

    完成页面背景和 div 边框

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,7 +7,7 @@ export const Home = () => {
     <div className="home">
       <header style={{backgroundImage: `url(${headerBg})`}}/>
       <main>
-        <section className="section1"></section>
+        <section className="section1">hi</section>
         <section className="section2"></section>
         <section className="section3"></section>
         <section className="section4"></section>
